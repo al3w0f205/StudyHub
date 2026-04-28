c3f9e949ded6c11ad80ff6374093433cc3ac137d
al3w0f <miguel.alejandro.figueroa@gmail.com>
2026-04-28 01:04:23 -0500

    d e2bdhfbh3rfbhj3bfhj

--- scripts/create-admin.js
+++ scripts/create-admin.js
@@ -6,10 +6,14 @@
  */
 
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 import bcrypt from "bcryptjs";
 import { createInterface } from "readline";
 
-const prisma = new PrismaClient();
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 function prompt(question) {
   const rl = createInterface({ input: process.stdin, output: process.stdout });
